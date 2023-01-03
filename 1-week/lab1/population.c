fa3e921b739e1eba0958de2d209ac90bceafca30
Rodrigo Lins <rlinsdev@gmail.com>
2023-01-03 11:12:07 -0300

    Population

--- 1-week/lab1/population.c
+++ 1-week/lab1/population.c
@@ -1,23 +1,41 @@
-// #include <cs50.h>
+#include <cs50.h>
 #include <stdio.h>
 
-
-// TODO:Lins: Just integers
-
+//gcc -g population.c -lcs50 -o population
+int calc_llamas(int start_size, int end_size);
 
 int main(void)
 {
-	printf("Hi");
-    // // TODO: Prompt for start size
-	// int start_size = get_int("Start Size: ");
+	int start_size = get_int("Start size: ");
+	int end_size = get_int("End size: ");
+	int years = calc_llamas(start_size, end_size);
+	printf("Years: %i\n", years);
+}
 
-    // // TODO: Prompt for end size
-	// int end_size = get_int("End size: ");
+// Validar numero negativo
+// validar um menor que o outro?
 
-	// int new_llamas = start_size / 3;
-	// int p_away_llamas = start_size / 4;
+/**
+ * @brief Calculate the number of years a population of llamas will grow, based
+ * in a n/3 born and n/4 deaths, by year.
+ * @param start_size Start size of the population
+ * @param end_size Final size of the population
+ * @return int - Number of years
+ */
+int calc_llamas(int start_size, int end_size)
+{
+	int years = 0;
+	int ll_by_year = start_size;
 
-    // TODO: Calculate number of years until we reach threshold
+	do
+	{
+		int new_llamas = start_size / 3;
+		int p_away_llamas = start_size / 4;
+		ll_by_year = start_size + (new_llamas - p_away_llamas);
 
-    // TODO: Print number of years
+		years++;
+		start_size = ll_by_year;
+	} while (end_size >= ll_by_year);
+
+	return years;
 }
