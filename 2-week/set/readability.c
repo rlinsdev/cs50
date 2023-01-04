7c422222ffea18ac844a9484b5a308bd424add6e
Rodrigo Lins <rlinsdev@gmail.com>
2023-01-04 11:08:38 -0300

    Ctype enabled

--- 2-week/set/readability.c
+++ 2-week/set/readability.c
@@ -1,5 +1,6 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <ctype.h>
 
 static void handle_text(string txt, int *qtd_ltt, int *qtd_stc, int *qtd_wrd);
 static void handle_calc(int qtd_ltt, int qtd_stc, int qtd_wrd);
@@ -11,11 +12,19 @@ int main(void)
     int qtd_wrd = 1;
     string text = get_string("Text: ");
 
-    // Grade 3  65 letters, 4 sentences, and 14 words
+    // // Grade 3  65 letters, 4 sentences, and 14 words
     // string text = "Congratulations! Today is your day. You're off to Great Places! You're off and away!";
 
-    // // Grade 5. 214 letters, 4 sentences, and 56 words
+    // Grade 5. 214 letters, 4 sentences, and 56 words
     // string text = "Harry Potter was a highly unusual boy in many ways. For one thing, he hated the summer holidays more than any other time of year. For another, he really wanted to do his homework, but was forced to do it in secret, in the dead of the night. And he also happened to be a wizard.";
+
+    // string text = "Alice was beginning to get very tired of sitting by her sister on the bank, and of having nothing to do: once or twice she had peeped into the book her sister was reading, but it had no pictures or conversations in it, 'and what is the use of a book,' thought Alice 'without pictures or conversation?'";
+
+    // //250 letter 55 words
+    // string text = "It was a bright cold day in April, and the clocks were striking thirteen. Winston Smith, his chin nuzzled into his breast in an effort to escape the vile wind, slipped quickly through the glass doors of Victory Mansions, though not quickly enough to prevent a swirl of gritty dust from entering along with him.";
+
+    // 295 letters 70 words 3 sentences
+    // string text = "When he was nearly thirteen, my brother Jem got his arm badly broken at the elbow. When it healed, and Jem's fears of never being able to play football were assuaged, he was seldom self-conscious about his injury. His left arm was somewhat shorter than his right; when he stood or walked, the back of his hand was at right angles to his body, his thumb parallel to his thigh.";
 
     // Call method to parse the sentence
     handle_text(text, &qtd_ltt, &qtd_stc, &qtd_wrd);
@@ -66,8 +75,7 @@ static void handle_text(string txt, int *qtd_ltt, int *qtd_stc, int *qtd_wrd)
     int i = 0;
     while (txt[i])
     {
-        // Simulate isAlpha func.
-        if((txt[i] >= 'a' && txt[i] <= 'z') || (txt[i] >= 'A' && txt[i] <= 'Z'))
+        if (isalpha(txt[i]))
         {
             // +1 Letter
             *qtd_ltt = *qtd_ltt + 1;
