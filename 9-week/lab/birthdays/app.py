7a57cc41f380f1474e8ca98801106a798551757a
Rodrigo Lins <rlinsdev@gmail.com>
2023-01-13 14:30:49 -0300

    9-Week - lab - birth - Post - Final commit

--- 9-week/lab/birthdays/app.py
+++ 9-week/lab/birthdays/app.py
@@ -26,9 +26,15 @@ def after_request(response):
 def index():
     if request.method == "POST":
 
-        # TODO: Add the user's entry into the database
+        # Get data from form
         name = request.form.get("name")
+        month = request.form.get("month")
+        day = request.form.get("day")
 
+        # Insert into database
+        db.execute("INSERT INTO birthdays (name, month, day) VALUES (?, ?, ? )", name, month, day)
+
+        # Refresh page
         return redirect("/")
 
     else:
