e3606aaee85e9ed43afabab8c02e0d9d97d2a006
Rodrigo Lins <rlinsdev@gmail.com>
2023-01-18 18:11:37 -0300

    Rollback app.js file

--- project/app.js
+++ project/app.js
@@ -1,45 +1,41 @@
 const express = require('express');
-// const authRoutes = require('./routes/auth-routes');
-// const profileRoutes = require('./routes/profile-routes');
-// const passportSetup = require('./config/passport-setup');
-// const mongoose = require('mongoose');
-// // const keys = require('./config/keys');
-// const cookieSession = require('cookie-session');
-// const passport = require('passport');
-// require('dotenv').config();
+const authRoutes = require('./routes/auth-routes');
+const profileRoutes = require('./routes/profile-routes');
+const passportSetup = require('./config/passport-setup');
+const mongoose = require('mongoose');
+// const keys = require('./config/keys');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+require('dotenv').config();
 
 const app = express();
 
-// // View Engine
-// app.set('view engine','ejs');
+// View Engine
+app.set('view engine','ejs');
 
-// app.use(cookieSession({
-//     maxAge: 24 * 60 * 60 * 1000,  // Expiration = 1 day
-//     keys:[process.env.SES_COOKIE]
-// }));
+app.use(cookieSession({
+    maxAge: 24 * 60 * 60 * 1000,  // Expiration = 1 day
+    keys:[process.env.SES_COOKIE]
+}));
 
-// // Initialize passport
-// app.use(passport.initialize());
-// // Initialize session cookie
-// app.use(passport.session());
+// Initialize passport
+app.use(passport.initialize());
+// Initialize session cookie
+app.use(passport.session());
 
-// // Mongo
-// mongoose.set('strictQuery', true);
-// mongoose.connect(process.env.MON_dbURI, () => {
-//     console.log('Mongo connected');
-// });
+// Mongo
+mongoose.set('strictQuery', true);
+mongoose.connect(process.env.MON_dbURI, () => {
+    console.log('Mongo connected');
+});
 
-// // Point to routes in folder
-// app.use('/auth', authRoutes);
-// app.use('/profile', profileRoutes);
+// Point to routes in folder
+app.use('/auth', authRoutes);
+app.use('/profile', profileRoutes);
 
-// // // localhost:3000
-// app.get('/', (req, res) => {
-//     res.render('home');
-// })
-
+// // localhost:3000
 app.get('/', (req, res) => {
-    res.json({message:"Hello from express"});
+    res.render('home');
 })
 
 app.listen(3030, () => {
