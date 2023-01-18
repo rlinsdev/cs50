9a984c6ae8760ccb2e7348344c43f8ef76939df4
Rodrigo Lins <rlinsdev@gmail.com>
2023-01-18 18:46:21 -0300

    Changes based in a article

--- project/index.js
+++ project/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const authRoutes = require('../routes/auth-routes');
-const profileRoutes = require('../routes/profile-routes');
-const passportSetup = require('../config/passport-setup');
+const authRoutes = require('./routes/auth-routes');
+const profileRoutes = require('./routes/profile-routes');
+const passportSetup = require('./config/passport-setup');
 const mongoose = require('mongoose');
 // const keys = require('./config/keys');
 const cookieSession = require('cookie-session');
